078f6d1c71bd32d951fe3ccbd112d765d2a55047
WasathTheekshana <wasath.vt@gmail.com>
2024-02-25 19:37:24 +0530

    chore: Initial commit

---
+++ src/config/express.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import v1 from "../api/routes/v1";
+
+dotenv.config();
+
+const app = express();
+
+// log requests -> dev: console || prod : file
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+// mount routes
+app.use("/api/v1", v1);
+
+
+export { app };
