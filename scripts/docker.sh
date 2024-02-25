5e22923fcc4268d5c5d5266bf91c40302c72dd7b
WasathTheekshana <wasath.vt@gmail.com>
2024-02-25 19:57:25 +0530

    ci: Docker related files 🐬

---
+++ scripts/docker.sh
@@ -0,0 +1,13 @@
+echo "Unite | Docker CLI"
+echo "-----------------------------------------"
+
+if [ "$1" == "build" ]; then
+    docker build -f Dockerfile -t unite-doctor-backend ../
+    elif [ "$1" == "run" ]; then
+    docker run -it -p 8080:8080 unite-doctor-backend
+    elif [ "$1" == "help" ]; then
+    echo "build: builds the docker image"
+    echo "run: runs the docker image"
+else
+    echo "Please enter a valid command"
+fi
