32164fd4e647d1481f245977725b5c57a8f120aa
WasathTheekshana <wasath.vt@gmail.com>
2024-02-25 22:34:56 +0530

    refactor: Remove the env util and add .env directly

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { app } from "./config/express";
-import env from "./utils/validate-env";
 import dotenv from "dotenv";
+import { app } from "./config/express";
+import { connect } from "./config/db";
 
 dotenv.config();
 
-const HOST = env.HOST;
-const PORT = env.PORT;
+const HOST = process.env.HOST!;
+const PORT = parseInt(process.env.PORT!);
 
+connect();
 app.listen(PORT, HOST, () => {
 	console.log(`Unite Doctor Backend is running on http://${HOST}:${PORT} 🚀`);
 });
