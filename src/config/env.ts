16440fddd1a75d517e2778548214695e69dffd03
WasathTheekshana <wasath.vt@gmail.com>
2024-02-26 07:02:10 +0530

    refactor: Env move into seperate file

---
+++ src/config/env.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import path from "path";
+
+// Import .env variables
+require("dotenv-safe").config({
+	path: path.join(__dirname, "../../.env.example"),
+});
+
+export default {
+	mongo: {
+		uri:
+            process.env.MONGO_CONNECTION_STRING
+	},
+	isDev: process.env.NODE_ENV === "dev",
+	host: process.env.HOST,
+	port: process.env.PORT,
+};
