dc0bfe5559b23380cecb6eb3ccb7191b71f8c7b0
WasathTheekshana <wasath.vt@gmail.com>
2024-02-27 16:31:56 +0530

    feat: All doctors endpoint

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,12 @@
 // errors
 export { IErrorDetails } from "./error";
 export { IAPIError } from "./error";
+
+// Doctor
+export {
+  IDoctor,
+  IDoctorMethods,
+  ITransformedDoctor,
+  IDoctorModel,
+  IList
+} from "./doctor/doctor";
