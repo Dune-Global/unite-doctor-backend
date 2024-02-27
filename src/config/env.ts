a34396319fb8cd47d0005e69a5713b98d5d4a58a
WasathTheekshana <wasath.vt@gmail.com>
2024-02-27 16:31:06 +0530

    feat: Bcrypt rounds to the env file

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,17 +1,16 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import path from "path";
 
 // Import .env variables
 require("dotenv-safe").config({
-	path: path.join(__dirname, "../../.env.example"),
+  path: path.join(__dirname, "../../.env.example"),
 });
 
 export default {
-	mongo: {
-		uri:
-            process.env.MONGO_CONNECTION_STRING
-	},
-	isDev: process.env.NODE_ENV === "dev",
-	host: process.env.HOST,
-	port: process.env.PORT,
+  mongo: {
+    uri: process.env.MONGO_CONNECTION_STRING,
+  },
+  isDev: process.env.NODE_ENV === "dev",
+  host: process.env.HOST,
+  port: process.env.PORT,
+  bcryptRounds: 10,
 };
