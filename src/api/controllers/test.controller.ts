181ae853c555049257a08565fa6fa6f92a7ca22a
WasathTheekshana <wasath.vt@gmail.com>
2024-02-27 16:30:01 +0530

    refactor: Delete test routes

--- src/api/controllers/test.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-// test route
-export const test = (_req: Request, res: Response) => {
-	res.json({
-		message: "Hello from testing route ✅",
-	});
-};
