b8b4e9c60b61efb47551869b14755a9e13af9f50
WasathTheekshana <wasath.vt@gmail.com>
2024-02-27 16:31:30 +0530

    feat: Doctor related types and interfaces

---
+++ src/types/doctor/doctor.d.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+export interface ITransformedDoctor {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  slmcNumber: string;
+  mobile: number;
+  isEmailVerified: boolean;
+  isSlmcVerified: boolean;
+
+  transform(): Partial<ITransformedDoctor>;
+}
+
+export interface IDoctor {
+  firstName: string;
+  lastName: string;
+  email: string;
+  slmcNumber: string;
+  mobile: number;
+  password: string;
+  isEmailVerified: boolean;
+  isSlmcVerified: boolean;
+}
+
+export interface IDoctorMethods {
+  passwordMatches(password: string, pwhash: string): any;
+  transform(this: mongoose.Document): Partial<ITransformedDoctor>;
+}
+
+export interface IList {
+  page?: number;
+  perPage?: number;
+  name?: string;
+  email?: string;
+}
+
+export interface IDoctorModel
+  extends mongoose.Model<IDoctor, {}, IDoctorMethods> {
+  list: (options: IList) => Promise<ITransformedDoctor[]>;
+  checkDuplicateEmail: (error: any) => APIError;
+}
