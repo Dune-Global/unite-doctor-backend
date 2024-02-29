9c75cb7821eeb36887a6f0d220a1472e895b45cc
WasathTheekshana <wasath.vt@gmail.com>
2024-02-29 16:50:11 +0530

    refactor: Add enums router

--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import doctorRouter from "./doctor.route";
 import refreshRouter from "./refreshToken.route";
+import enumsRouter from "./enums.route";
 const router = express.Router();
 
 // Test route for check overall connectivity
@@ -11,5 +12,8 @@ router.get("/tryme", (_req: Request, res: Response) => {
 // Plug the router into the express app
 router.use("/doctor", doctorRouter);
 router.use("/token", refreshRouter);
+
+// common routes
+router.use("/common", enumsRouter);
 
 export default router;
