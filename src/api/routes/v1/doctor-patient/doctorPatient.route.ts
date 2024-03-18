73c9847e43d7bfcd17f9c614c5d94ec94e7f13df
yasithranusha <yasithranusha24@gmail.com>
2024-03-19 03:16:40 +0530

    feat: Doctor patient connection

--- src/api/routes/v1/doctor-patient/doctorPatient.route.ts
+++ src/api/routes/v1/doctor-patient/doctorPatient.route.ts
@@ -1,9 +1,27 @@
 import express from "express";
-import { connectPatientDoc } from "../../../controllers/doctor-patient/doctorPatient.controller";
-import { isAuthPatient } from "./../../../../middleware/auth";
+import {
+  connectPatientDoc,
+  addPrescription,
+  getConnectedDoctors,
+  getConnectedPatients,
+  getDoctorPatientDetail,
+} from "../../../controllers/doctor-patient/doctorPatient.controller";
+import {
+  isAuthDoctor,
+  isAuthDoctorOrPatient,
+  isAuthPatient,
+} from "./../../../../middleware/auth";
 
 const router = express.Router();
 
 router.get("/connect/:doctorId", isAuthPatient, connectPatientDoc);
+router.post("/add-prescription/:patientId", isAuthDoctor, addPrescription);
+router.get(
+  "/doctor-patient-detail/:patientSessionId",
+  isAuthDoctorOrPatient,
+  getDoctorPatientDetail
+);
+router.get("/connected-patients", isAuthDoctor, getConnectedPatients);
+router.get("/connected-doctors", isAuthPatient, getConnectedDoctors);
 
 export default router;
