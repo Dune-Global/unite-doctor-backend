f2246176eebe69fbd0e305cb091f9d0cf82ce5c8
yasithranusha <yasithranusha24@gmail.com>
2024-03-19 01:57:32 +0530

    refactor: cors

--- src/config/express.ts
+++ src/config/express.ts
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use(cors(
     {
         origin: "http://localhost:3000",
+        credentials: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     }
 ));
 
